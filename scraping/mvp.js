85be4f673f9d04d5625525aaee2f131dd15bfc76
JoseG04 <jgcode04@gmail.com>
2023-01-11 11:22:54 -0500

    Add scrape for mvp from kings league webpage

---
+++ scraping/mvp.js
@@ -0,0 +1,46 @@
+import { TEAMS, writeDBFile } from '../db/index.js'
+import { MVP_PAGE, cleanText } from './utils.js'
+
+async function getMVP() {
+    const $ = await MVP_PAGE
+    const $rows = $('table tbody tr')
+
+    const MVP_SELECTORS = {
+        rank: { selector: '.fs-table-text_1', typeOf: 'number' },
+        team: { selector: '.fs-table-text_3', typeOf: 'string' },
+        player: { selector: '.fs-table-text_4', typeOf: 'string' },
+        games: { selector: '.fs-table-text_5', typeOf: 'number' },
+        mvps: { selector: '.fs-table-text_6', typeOf: 'number' }
+    }
+
+    function getImageFromTeam({ name }) {
+        const { image } = TEAMS.find(team => team.name === name)
+        return image
+    }
+
+    const mvps = []
+    $rows.each((_, item) => {
+        const $item = $(item)
+
+        const mvpEntries = Object.entries(MVP_SELECTORS)
+            .map(([key, { selector, typeOf }]) => {
+                const rawValue = $item.find(selector).text()
+                let cleanValue = cleanText(rawValue);
+
+                const value = typeOf === 'number'
+                    ? Number(cleanValue)
+                    : cleanValue
+
+                return [key, value]
+            })
+
+        let mvpObject = Object.fromEntries(mvpEntries)
+        mvpObject.image = getImageFromTeam({ name: mvpObject.team })
+        mvps.push(mvpObject)
+    })
+
+    return mvps
+}
+
+const mvps = await getMVP()
+await writeDBFile('mvp', mvps)
